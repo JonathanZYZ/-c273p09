73513c7958150305a6feeb71feafadb47d01e50c
19021227 <19021227@10.174.122.202>
2021-01-11 15:00:37 +0800

    Test22

--- js/showCountryObese.js
+++ js/showCountryObese.js
@@ -8,9 +8,9 @@ $(document).ready(function () {
             cache: false,
             dataType: "JSON",
             success: function (response) {
-                var message = "<tbody><tr><td>" + response[0].population + "</td><td>" + response[0].obese + "</td></tr></tbody>";
+                var message = "<tr><td>" + response[0].population + "</td><td>" + response[0].obese + "</td></tr>";
 
-                $("#obeseTable").html(message);
+                $("#obeseTable tbody").html(message);
             },
             error: function (obj, textStatus, errorThrown) {
                 console.log("Error " + textStatus + ": " + errorThrown);
