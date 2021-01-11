325c1ffe5d8913c34d99eab8165fa417005025a0
19021227 <19021227@10.174.122.202>
2021-01-11 15:08:48 +0800

    Test222

--- js/showCountryObese.js
+++ js/showCountryObese.js
@@ -5,15 +5,11 @@ $(document).ready(function () {
             type: "GET",
             url: "getCountryDetails.php",
             data: "id=" + id,
-            cache: false,
             dataType: "JSON",
             success: function (response) {
-                var message = "<tr><td>" + response[0].population + "</td><td>" + response[0].obese + "</td></tr>";
+                var message = "<tbody><tr><td>" + response[0].population + "</td><td>" + response[0].obese + "</td></tr></tbody>";
 
-                $("#obeseTable tbody").html(message);
-            },
-            error: function (obj, textStatus, errorThrown) {
-                console.log("Error " + textStatus + ": " + errorThrown);
+                $("#obeseTable").append(message);
             }
 
         });
